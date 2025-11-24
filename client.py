a77c5a7223078ff098eb3273d667d31070ab46c8
daimqureshi644 <daimqureshi2577@gmail.com>
2025-11-24 12:56:36 +0500

    Add files via upload

---
+++ client.py
@@ -0,0 +1,16 @@
+import google.generativeai as genai
+
+client = genai (
+   api_key= "AIzaSyASZbzPPpBHYoxNkxHSwyKHviyI-hTeXzQ"
+)
+command ='''
+ hello
+
+'''
+completion = client.chat.completions.create(
+    model="gemini-2.0-flash",
+    messages=[
+        {"role":"system","content":"You are a person name Daim Qureshi who speak urdu and english. You are from Pakistan. You analyze the chat hisory and respond like Daim",
+        "role": "user","content":command}]
+)
+print(completion.choices[0].message.content)
